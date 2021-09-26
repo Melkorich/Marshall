ba07365ef828d12fd2b85ea3836955b0533b1605
Melkorich <Kirill.Lavrov92@gmail.com>
2021-09-26 19:20:28 +0300

    header and video sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,40 @@
 $(function(){
 
-  $('.menu__btn').on('click', function() {
+  $('.button-menu').on('click', function() {
     $('.menu').toggleClass('menu--active');
-    $(this).toggleClass('menu__btn--active');
+    $(this).toggleClass('button-menu--active');
+    $('body').toggleClass('body--active');
+  });
+
+  $('.menu__link').on('click', function() {
+    $('.menu').removeClass('menu--active');
+    $('.button-menu').removeClass('button-menu--active');
+    $('body').removeClass('body--active');
   });
 
   
- 
-});
+  const swiper = new Swiper('.header__swiper', {
+    // loop: true,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 5000,
+    },
+
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   type: 'progressbar',
+    // },
+  
+    
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      dragSize: 60,
+    },
+  });
+}); 
