a3aaa696d7c71a070a1193b9daba625a28de80c0
Melkorich <Kirill.Lavrov92@gmail.com>
2022-01-22 18:19:17 +0300

    btn upload fixe

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ function scripts(){
     'node_modules/rateyo/src/jquery.rateyo.js',
     'node_modules/swiper/swiper-bundle.js',
     'app/js/main.js'
-  ])
+  ]) 
   .pipe(concat('main.min.js'))
   .pipe(uglify())
   .pipe(dest('app/js'))
